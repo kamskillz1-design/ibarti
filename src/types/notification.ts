67f37550a52ff41a77e045ce771f72cf22dbc335
kamskillz1-design <kamskillz1@gmail.com>
2026-09-04 10:39:07 +0200

    Add NotificationType and AppNotification interface

--- src/types/notification.ts
+++ src/types/notification.ts
@@ -1,1 +1,15 @@
+export type NotificationType =
+  | "trade_proposed"
+  | "trade_accepted"
+  | "trade_rejected"
+  | "trade_message"
+  | "trade_completed";
 
+export interface AppNotification {
+  id: string;
+  userId: string;
+  type: NotificationType;
+  relatedTradeId: string | null;
+  readAt: string | null;
+  createdAt: string;
+}
